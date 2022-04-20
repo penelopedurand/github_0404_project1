b535e57db8cf53ce41806eae4087232f5958f1e5
Robert Carroll <robertcarrollsales@gmail.com>
2022-04-19 21:58:23 -0400

    re-saved main

--- Javascript/index.js
+++ Javascript/index.js
@@ -4,25 +4,6 @@
 // Submit form for likes (user can “like” the brewery)
 // Submit a form to leave a review of the brewery.
 
-document.addEventListener('DOMContentLoaded', () => {
-
-    const arrBrew = "https://api.openbrewerydb.org/breweries"
-
-    fetch(arrBrew)
-    .then(resp => resp.json())
-    .then(data => {
-
-        function breweryName(name) {
-            const container = document.getElementById("brewery-list")
-            const arrName = document.createElement('name')
-
-        arrBrew.forEach(breweryName => renderName(breweryName));
-
-        }
-    
-   
-
-})})
 const individBrew = (arr) => {
     arr.forEach(brewObj => {
     const div = document.querySelector('.logo')
