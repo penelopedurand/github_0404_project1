4fa242394aaf04727a878709bc42cbedc4edb4b8
Robert Carroll <robertcarrollsales@gmail.com>
2022-04-19 22:01:43 -0400

    saved-main

--- Javascript/index.js
+++ Javascript/index.js
@@ -3,6 +3,12 @@
 // Filter out breweries by state via drop down menu (submit event)
 // Submit form for likes (user can “like” the brewery)
 // Submit a form to leave a review of the brewery.
+window.addEventListener('DOMContentLoaded', () => {
+    fetch("https://api.openbrewerydb.org/breweries")
+    .then(resp => resp.json())
+    .then(arrBrew => individBrew(arrBrew))
+});
+
 
 const individBrew = (arr) => {
     arr.forEach(brewObj => {
